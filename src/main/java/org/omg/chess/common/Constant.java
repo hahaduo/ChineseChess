fb13c8541a8822c9ec28826a85b4f336719fdf15
hahaduo <hahaduo@gmail.com>
2015-01-25 23:47:30 +0800

    2015-01-25 23:47:23

---
+++ src/main/java/org/omg/chess/common/Constant.java
@@ -0,0 +1,9 @@
+package org.omg.chess.common;
+
+/**
+ * Created by lihongjun on 15/1/24.
+ */
+public class Constant {
+    public static int CHESS_COLOR_BALCK=0;
+    public static int CHESS_COLOR_RED=1;
+}
